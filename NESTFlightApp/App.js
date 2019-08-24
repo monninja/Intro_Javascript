3b55b23ff2a5687f2d068b34360a36680e93047f
Josue Leo Ortiz <Josue@Wat-Macbook-Venus-3.local>
2019-08-23 17:09:33 -0700

    expo app wrapper

---
+++ NESTFlightApp/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Image, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image
+          style={{width: 500, height: 500}}
+          source={require('./assets/NF_logo-01.png')}
+          //source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
+        />
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
