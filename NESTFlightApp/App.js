c474c5a01a42eb5e555866c5e1112f3064e78ae4
Monica Gutierrez <monica.gl@digitalnest.com>
2019-08-23 17:19:58 -0700

    This is new updates to the splash

--- NESTFlightApp/App.js
+++ NESTFlightApp/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import { StyleSheet, Image, Text, View } from 'react-native';
+import { StyleSheet, Image, ImageBackground, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image
-          style={{width: 500, height: 500}}
-          source={require('./assets/NF_logo-01.png')}
-          //source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
-        />
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <View style={styles.container} sty>
+      <ImageBackground style={styles.container}/>
+      <Image 
+        source={require('./assets/NF_logo-01.png')} 
+        style={{
+          position: 'absolute',
+          justifyContent: 'center',        
+          width: 700, 
+          height: 700, 
+          alignSelf:'center',}}/>
+      <Text 
+        style={{
+          color: 'white',
+          position: 'relative',
+          paddingBottom:'15%', 
+          }}> Supported by the DigitalNEST </Text>
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0D0D58',
     alignItems: 'center',
     justifyContent: 'center',
   },
